47e8092c0747615261f158a08d2f51d6420c71df
nphaxayaseng <nphaxayaseng@gmail.com>
2021-02-16 22:21:40 -0800

    encrypted password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,22 @@
 const User = require('../models/Users');
-const {users} = require('../data/users')
+const { users } = require('../data/users');
 
-const seedUsers = (req, res) => {
-    User.create(users)
-    .then(users => res.status(200).send(users))
-    .catch(err => res.status(500).send({ Error: err.message }));
+const {encryptPassword} = require('../utils/encryption')
+
+const seedUsers = async (req, res) => {
+
+    const hashedUsers = []
+    for (let i= 0; i < users.length; i++) {
+        
+        hashedUsers[i] = { ...users[i], password: await encryptPassword(users[i].password) }
+    }
+    console.table(hashedUsers)
+
+
+
+    User.create(hashedUsers)
+        .then(users => res.status(200).send(users))
+        .catch(err => res.status(500).send({ Error: err.message }));
 };
 
-module.exports = {seedUsers};
+module.exports = { seedUsers };
